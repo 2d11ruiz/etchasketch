555b5e4b93ad9bc65e32860a956e919a2670b8b7
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2026-01-06 00:18:09 -0500

    Complete Etch a Sketch project
    Finalized the project by adding a button to the webpage that allows the
    user to edit the size of the grid by removing and redrawing a grid of a
    user specified size.

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.classList.add('boxstyle');
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
-    container.appendChild(square);
-}
+const drawGrid = (size) => {
+    for (let i = 0; i < size*size; i++){
+        const square = document.createElement("div");
+        square.classList.add('boxstyle');
+        square.style.flexBasis = (100/size) + "%";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+        container.appendChild(square);
+    }
+};
+
+drawGrid(16);
+
+const newgrid = document.querySelector(".newgrid");
+
+const gbutton = document.createElement("button");
+gbutton.textContent = "New Grid";
+gbutton.style.margin = "10px auto";
+gbutton.addEventListener("click", () => {
+    let ginput = prompt("Enter New Grid Size, Max is 64, Default is 16:");
+    if (ginput === null) return;
+    let gsize = parseInt(ginput);
+    if (!isNaN(gsize) && gsize > 0 && gsize <= 64) {
+        container.innerHTML = "";
+        drawGrid(gsize);
+    } else {
+        alert("Invalid Input. Please Enter a Number Between 1 and 64. Default 16 Size Grid Drawn.");
+        container.innerHTML = "";
+        drawGrid(16);
+    }
+
+});
+newgrid.appendChild(gbutton);
